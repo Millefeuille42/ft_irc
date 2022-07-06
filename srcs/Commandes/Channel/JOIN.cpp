7f977d9800b33899c87ee60504fd33921035e5ec
Millefeuille <mlabouri@student.42.fr>
2022-07-06 09:06:02 +0200

    Added names on join

--- srcs/Commandes/Channel/JOIN.cpp
+++ srcs/Commandes/Channel/JOIN.cpp
@@ -60,10 +60,10 @@ void SockServer::join(SockServer &srv, std::vector<std::string>& args, User& use
 			if (!srv._chans[it->first].getTopic().empty()) {
 				sendMessage(user.fd, TOPIC(user.nick, srv._chans[it->first].getName()) + srv._chans[it->first].getTopic() + "\n", std::cout);
 			}
-			//std::vector<std::string> a;
-			//a.push_back("NAMES"); a.push_back(srv._chans[it->first].getName());
-			//names(srv, a, user);
-			// TODO a rajouter une fois mode #channel fait
+			std::vector<std::string> a;
+			a.push_back("NAMES"); a.push_back(srv._chans[it->first].getName());
+			names(srv, a, user);
+			// TODO Cause un crash sans mode #channel
 		}
 		else {
 			if (itc->second.isMode('i') == true) {
@@ -76,10 +76,10 @@ void SockServer::join(SockServer &srv, std::vector<std::string>& args, User& use
 				if (!itc->second.getTopic().empty()) {
 					sendMessage(user.fd, TOPIC(user.nick, itc->second.getName()) + itc->second.getTopic() + "\n", std::cout);
 				}
-				//std::vector<std::string> a;
-				//a.push_back("NAMES"); a.push_back(itc->second.getName());
-				//names(srv, a, user);
-				// TODO a rajouter une fois mode #channel fait
+				std::vector<std::string> a;
+				a.push_back("NAMES"); a.push_back(itc->second.getName());
+				names(srv, a, user);
+				// TODO Cause un crash sans mode #channel
 			}
 			else { //Channel Non-Rejoins
 				sendMessage(user.fd, "You can't join this channel", std::cerr);
